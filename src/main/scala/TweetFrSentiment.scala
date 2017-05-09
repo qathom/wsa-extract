bd32163b51605294417e2d21393e2771dba05b25
Unknown <v.komplita@gmail.com>
2017-05-10 00:14:00 +0200

    SENTIMENT !!!!
    SENTIMENTS !!!!
    
    
    sentiment petit oubli

---
+++ src/main/scala/TweetFrSentiment.scala
@@ -0,0 +1,36 @@
+import scala.io.Source
+import org.apache.commons.lang3.StringUtils
+
+class TweetFrSentiment {
+
+  private val hashTagExp = """\B#\w*[a-zA-Z]+\w*""".r
+  private val mentionExp = """\B@\w*[a-zA-Z]+\w*""".r
+  private val wordExp = """[a-zA-Z]+""".r
+  private val sentiments = new scala.collection.mutable.HashMap[String, Int]()
+
+  for (line <- Source.fromFile("input/FEEL-1.csv").getLines()) {
+    if (!line.startsWith("id")) {
+      val sp = line.split(";")
+      var score: Int = 1
+      val eval = sp(2)
+      if (!sp(1).contains(" ")) {
+        if (sp(2).equals("negative")) {
+          score = (score * -1)
+        }
+        sentiments.put(StringUtils.stripAccents(sp(1)), score)
+      }
+    }
+  }
+
+  def getSentiment(tweet :String ): Unit = {
+    var words = wordExp.findAllIn(StringUtils.stripAccents(tweet).toLowerCase).toSet
+    //for (word <- words) println("The word is : " + word + "\nScore : " + sentiments.get(word).getOrElse(0).asInstanceOf[Int])
+    val r = words.map(word => (word, sentiments.get(word).getOrElse(3).asInstanceOf[Int])).filter( (t) => t._2 != 3)
+    val size = r.size
+    val sum = r.foldLeft(0.0)(_+_._2)
+    val mean: Double = sum / size
+    //println("\nThe sum is : " + sum)
+    //println("The mean is : " +mean)
+    return mean
+  }
+}
