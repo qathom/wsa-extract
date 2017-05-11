d29f396650303e573b5c9bedc56c3899c85e4605
Qathom <tony9295@gmail.com>
2017-05-11 13:57:10 +0200

    Multithreading [WIP]

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,54 @@
+import scala.concurrent.ops._
+
+
 /**
   * Main
   */
 
-object Main   {
+object Main {
 
   def main(args: Array[String]): Unit = {
     /*
      * transform will read the input file contained in input/ directory,
      * normalize them and then write in the output/ directory.
      */
-    val tn = new TweetNormalizer
-    tn.transform("tweets.json")
+    // val tn = new TweetNormalizer
+    // tn.transform("sample-300.json")
+    // val queue = new java.io.File("./input").listFiles.filter(_.getName.endsWith(".json"))
+
+    val thread1 = new Thread {
+      override def run: Unit = {
+        val tn = new TweetNormalizer
+        tn.transform("sample-300.json")
+      }
+    }
+    thread1.start
+
+    val thread2 = new Thread {
+      override def run: Unit = {
+        val tn = new TweetNormalizer
+        tn.transform("sample-300.json")
+      }
+    }
+    thread2.start
 
     /**
       * Show statistics from output such as
       * the ratio of political tweets
       */
+      /*
     val ts = new TweetStatistics
     ts.setStats()
     ts.showRatios()
+    */
 
     /*
      * run Spark: set data frame and make queries (SQL like)
      */
+    /*
     val sa = new SparkAnalysis
     sa.run()
     sa.stop()
+    */
   }
 }
