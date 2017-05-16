4be2d6b26674fb38b8733c882780d66f811acde7
Qathom <tony9295@gmail.com>
2017-05-16 20:01:34 +0200

    Update output of chart1

--- src/main/scala/SparkAnalysis.scala
+++ src/main/scala/SparkAnalysis.scala
@@ -11,7 +11,7 @@ class SparkAnalysis() {
 
   var session: SparkSession = null
 
-  def formatDate(enDate: String): String = {
+  private def formatDate(enDate: String): String = {
     val simpleDateFormat: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
     val date: Date = simpleDateFormat.parse(enDate);
     return new SimpleDateFormat("dd.MM").format(date)
@@ -145,36 +145,31 @@ class SparkAnalysis() {
 
     val resultDFTweetsRatioPerDate = spark.sql("Select date_format(cast(unix_timestamp(created_at, 'EEE MMM dd HH:mm:ss ZZZZZ yyyy') AS TIMESTAMP), 'yyyy-MM-dd') as Jour, candidate as Candidat ,Count(Distinct id_str) as NrbTweets, Round(Avg(sentiment)*10,2) as Sentiment FROM tweets WHERE candidate IS NOT NULL GROUP by candidate, date_format(cast(unix_timestamp(created_at, 'EEE MMM dd HH:mm:ss ZZZZZ yyyy') AS TIMESTAMP), 'yyyy-MM-dd') ORDER BY Jour ASC")
     resultDFTweetsRatioPerDate.show()
+*/
 
     /*
-     * build charts
+     * build the chart based on the JSON file
      */
     val bc = new ChartBuilder
-
-    // chart 1
     val tstat = new TweetStatistics
-    val json = tstat.getData()
+    val list = tstat.getDataListSorted()
 
     // x-axis represents the dates
     val x:Seq[String] =
-      for (i <- 0 to json.length() - 1)
-        yield formatDate(json.names().get(i).toString)
+      for (el <- list)
+        yield formatDate(el._1)
 
     // y1-axis represents not political tweets
     val y1:Seq[Double] =
-      for (i <- 0 to json.length() - 1)
-        yield new JSONObject(json.get(json.names().get(i).toString).toString).getDouble("notPolitics")
+      for (el <- list)
+        yield el._2.get("notPolitics").get
 
     // y2-axis represents political tweets
     val y2:Seq[Double] =
-      for (i <- 0 to json.length() - 1)
-        yield new JSONObject(json.get(json.names().get(i).toString).toString).getDouble("politics")
+      for (el <- list)
+        yield el._2.get("politics").get
 
     bc.buildTimeline(x, y1, y2)
-
-    // chart 2
-    bc.buildSentiments()
-*/
   }
 
   def stop(): Unit = {
